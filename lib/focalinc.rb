843ae90f0be4b86710c56af1b9d72d24156f06f7
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-06-07 10:56:22 -0300

    Rubocop

--- lib/focalinc.rb
+++ lib/focalinc.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "focalinc/version"
+require_relative 'focalinc/version'
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Focalinc
   class Error < StandardError; end
--- spec/focalinc_spec.rb
+++ spec/focalinc_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Focalinc do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Focalinc::VERSION).not_to be nil
   end
 end
