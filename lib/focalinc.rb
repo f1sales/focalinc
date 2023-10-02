251d27771920986d1f9110ac97619321fc1561a5
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-10-02 11:50:14 -0300

    F1API-909: Two leads for Focal one for Lopes

--- lib/focalinc.rb
+++ lib/focalinc.rb
@@ -67,9 +67,9 @@ module Focalinc
     class << self
       def switch_source(lead)
         @lead = lead
-        return "#{lead_source} - Focal" if Lead.count.even?
+        return "#{lead_source} - Lopes" if (Lead.count % 3).zero?
 
-        "#{lead_source} - Lopes"
+        "#{lead_source} - Focal"
       end
 
       private
